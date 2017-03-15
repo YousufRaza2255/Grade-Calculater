0dd00a268561dd0793a0e05d88daacb173924c3f
Yousuf Raza <Yousufraza223@gmail.com>
2017-03-15 20:12:13 +0500

    Add files via upload
    Grade Calculater

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+
+var per = +prompt('Enter Your Percentage');
+if (per>=80){
+    alert("your grade is A-1");
+}
+
+else if (per>=70){
+    alert("your grade is A");
+}
+
+else if (per>=60){
+    alert("your grade is B");
+}
+
+else if (per>=50){
+    alert("your grade is C");
+}
+
+else if (per>=40){
+    alert("your grade is D");
+}
+
+else if (per>=33){
+    alert("your grade is E");
+}
+
+else if (per<=33){
+    alert("your are Fail");
+}
+else  
+{
+    ("Enter a Valid Percentage");    };
